d7ff5c24547fe4944c2f05362896ed24d791aae2
Oriza Sativa Cikal.M <oryzasativacikal@gmail.com>
2023-08-22 17:02:30 +0700

    install library and setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Splide, SplideSlide } from '@splidejs/react-splide'
+import { SplideTrack } from '@splidejs/react-splide'
+import '@splidejs/react-splide/css';
+import { Justify } from 'react-bootstrap-icons';
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <Justify type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions" />
+
+      <div class="offcanvas offcanvas-start" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasWithBothOptionsLabel">Backdrop with scrolling</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+          <p>Try scrolling the rest of the page to see this option in action.</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Splide
+        options={{
+          rewind: true,
+          arrows: false,
+          type: 'loop',
+          autoplay: true,
+          perPage: 3,
+          gap: 10,
+        }}
+      >
+
+        <SplideSlide>
+          <p>SATU</p>
+        </SplideSlide>
+        <SplideSlide>
+          <p>DUA</p>
+        </SplideSlide>
+        <SplideSlide>
+          <p>TIGA</p>
+        </SplideSlide>
+        <SplideSlide>
+          <p>EMPAT</p>
+        </SplideSlide>
+        <SplideSlide>
+          <p>LIMA</p>
+        </SplideSlide>
+      </Splide>
+
     </>
   )
 }
